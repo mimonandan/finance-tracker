8693a720916d0189c9eec08db4cbe525c6e4c6b5
mimonandan <mimo.nandan@gmail.com>
2026-04-25 02:36:06 +0530

    Day 3: Implement Expense API with MVC structure and in-memory storage

---
+++ services/expenseService.js
@@ -0,0 +1,19 @@
+//services/expenseService.js - Buisness logic for the expense operations can be expanded here in the future if needed.
+// Service → handles logic and data manipulation.
+
+let expenses = [];
+
+export function addExpense(data) {
+  const newExpense = {
+    id: Date.now(),
+    createdAt: new Date().toISOString(),
+    ...data
+  };
+
+  expenses.push(newExpense);
+  return newExpense;
+}
+
+export function getExpenses() {
+  return expenses;
+}
